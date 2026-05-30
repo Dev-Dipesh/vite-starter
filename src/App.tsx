68eb20067417291288e80323fe6ccc406a7a94e5
Dev-Dipesh <itdeveloper03@gmail.com>
2026-05-30 17:30:08 +0000

    Implement confetti-counter

--- src/App.tsx
+++ src/App.tsx
@@ -62,10 +62,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+          <h1>Confetti Counter</h1>
         </div>
         <div className="counter-actions">
           <button
